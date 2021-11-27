dbe6c916bf3a5e0e33c54e8200eaaf3a9356765c
shyam130302 <shyam13rocks@gmail.com>
2021-11-27 18:03:45 +0530

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useGlobalContext } from '../context/Context';
+import Logo from '../img/nikelogo.svg';
+import Bag from '../img/bag.svg';
+
+function Navbar() {
+
+    const {toggleOpenCloseCart, amount} = useGlobalContext();
+
+    return (
+        <nav>
+            <div className='navbar-center'>
+                <img className='logo' src={Logo} alt='logo' width="200" height="150" style={{marginTop:"50px"}}/>
+                <h1 align="left">NIKE - Only Shoes</h1>
+                <div className='cart-bag' 
+                    onClick={_ => toggleOpenCloseCart('OPEN_CART')}>
+                    <img src={Bag} alt='cart bag' />
+                    <h4 className='amount'>Cart({amount})</h4>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
